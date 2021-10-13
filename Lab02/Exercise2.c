331032bef01e1904f14e6b56b9ca22e87af2fadb
Mina Nguyen <maihuongn638@gmail.com>
2021-10-13 20:59:07 +0700

    Exercies 02

---
+++ Lab02/Exercise2.c
@@ -0,0 +1,27 @@
+/*
+2. Write a function to calculate the factorial with given integer n,
+which satisfies the following:
+	if n is odd, factorial = 1 * 3 * 5 * … * n
+	if n is even, factorial = 2 * 4 * 6 * … * n
+Ex:
+______________________________________
+| Input: 11                          |
+| Output: 10395                      |
+|____________________________________|
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+	//testing variable, applying it to your algorithm for auto-evaluating
+	int n = atoi(argv[1]);
+	int result = 1;
+	//Your codes here
+	int idx = n % 2 == 0 ? 2 : 1;
+    for(; idx <= n; idx += 2)
+        result *= idx;
+	printf("%d", result);
+	return 0;
+}
